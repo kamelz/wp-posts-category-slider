47d1539d2aa67375237004fd7932a9f657a61ba6
Kamelz <kamel1145@gmail.com>
2018-09-26 16:11:33 +0300

    3 posts per slider

--- assets/js/posts-categories-slider-script.js
+++ assets/js/posts-categories-slider-script.js
@@ -2,7 +2,16 @@ var $j = (typeof $ !== 'undefined')? $:jQuery;
 
 $j(document).ready(function(){
 
- var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -10,3 +19,5 @@ $j(document).ready(function(){
     });
 
 });
+
+
